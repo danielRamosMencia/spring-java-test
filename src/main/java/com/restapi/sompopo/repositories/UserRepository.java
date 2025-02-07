2670927d1b3db7ac40a145162b5ab8c9bf8bb2ea
danielRamosMencia <danieldermramos@gmail.com>
2025-02-07 17:46:13 -0600

    feat: forget

--- src/main/java/com/restapi/sompopo/repositories/UserRepository.java
+++ src/main/java/com/restapi/sompopo/repositories/UserRepository.java
@@ -4,7 +4,6 @@ import com.restapi.sompopo.entitites.UserEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
-import com.restapi.sompopo.dtos.AllUsersDto;
 import java.util.List;
 
 import java.util.Optional;
